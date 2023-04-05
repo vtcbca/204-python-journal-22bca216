32eac4897b82aa8a48dd538b911e4dc83bf5f495
kailash shahu <127926917+22bca216@users.noreply.github.com>
2023-04-05 11:31:34 +0530

    script 1 solution.py

---
+++ string solution/script1solution.py
@@ -0,0 +1,7 @@
+ #python script to check string is palindrome or not
+k=input("Enter any string :")
+a=k[::-1]
+if(k==a):
+    print("String is palindrome !")
+else:
+    print("String is not palindrome !")
